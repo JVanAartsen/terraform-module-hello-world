8e1e267d542c47bdc90307f46f32b7771ca44245
Josh Van Aartsen <vanaartsenj@gmail.com>
2023-04-01 18:21:20 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "always_diff" {
   type = bool
   default = false
 }
+
+
+variable "some_required_string" {
+  type = string
+}
+
+variable "some_defaulted_string" {
+  type = string
+  default = "I AM A DEFAULT!"
+}
